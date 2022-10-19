d39322dc1e8d919d3d9c9c545682aa8919714407
Duong Tran My Linh <s3938101@rmit.edu.vn>
2022-10-19 13:06:16 +0700

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,38 +1,38 @@
-const logotext = "Mei Lin";
+const logotext = "Meilin";
 const meta = {
     title: "Duong ",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    description: "a penultimate year IT major at RMIT University.",
 };
 
 const introdata = {
     title: "Duong Tran My Linh | Yuan Mei Lin",
     animated: {
-        first: "IT consultant | Back-end Developer | Data Analyst.",
+        first: "IT consultant | Software Developer | Data Analyst.",
         second: "Implement DevOps practice for team.",
-        third: "Design AI/ML/Cloud solutions & systems.",
+        third: "Utilise AI/ML/Cloud solutions & high-level systems.",
     },
-    description: "Programming",
-    your_image_url: "https://solarsystem.nasa.gov/gltf_embed/2355",
+    description: "Programming is an art, and hence, programmers are artists. It is imagination and ingenuity manifest; pure creation branded with each developer’s own individual style.",
+    your_image_url: "https://i.postimg.cc/XNcVXPW0/IMG-1338.jpg",
 };
 
 const dataabout = {
     title: "Summary",
-    aboutme: "2nd year IT major student (AQF 3-year Program) at Dean School of Science, Engineering and Technology, RMIT University. My qualities as a Software Engineer include proven team-leading skills, automated/AI/ML deployment, logical tech-stack structuring as well as steady knowledge of on-trend technologies, engaged relationship-building between members, customers and last but not least, a good sense of humour! Extremely inquisitive,willing to learn and improve myself from inspiring peers in Tech-industry and through the process of working in profesionnal environment..",
+    aboutme: "Greetings, I am My Linh or Meilinn, your extroverted, far-sighted, result-oriented Data Engineer/IT Consultant, that differs myself from the majority people in the tech industry thanks to my good sense of humour and being a women in STEM.I have hands-on experience in leading cross-cultural teams, automating pipeline with AI/Machine Learning/Deep learning approach in Hackathon Competitions.", 
 };
 const activities = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Women Techmaker Ambassador",
+        where: "Google Developer Club",
+        date: "Sep. 2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Technical Lead | Data/AI Engineer",
+        where: "Top 3 team of Global Health Hackathon",
+        date: "Aug. 2022",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        jobtitle: "Project Manager | AI Engineer ",
+        where: "Shecodes Hackathon",
+        date: "Sep. 2022",
     },
 ];
 
@@ -58,22 +58,8 @@ const skills = [{
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-];
-
 const dataportfolio = [{
-        img: "https://i.postimg.cc/6pgYFN2S/IMG-1092.jpg",
+        img: "https://i.postimg.cc/xTvbf5xk/IMG-1309.jpg",
         desctiption: "AI-based Real-time Sign Language Translator with Machine Learning",
         link: "#",
     },
